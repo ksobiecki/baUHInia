e4ebf3a2da25aeee68ac76c4933feb462d39dd20
Jarosław Drzymalski <jarek.drzymalski@gmail.com>
2020-11-14 00:08:33 +0100

    Dodałem klase do autoryzacji (AuthInfo)
    Dodałem klasę do autoryzacji :)

---
+++ baUHInia/Authorisation/AuthInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baUHInia.Authorisation
+{
+    class AuthInfo
+    {
+        private AuthInfo() { }
+        private static AuthInfo _instance;
+
+        public int UserID { get; set; }
+        public String name { get; set; }
+        public String hash { get; set; }
+        public Boolean isAdmin { get; set; }
+
+
+        public static AuthInfo GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new AuthInfo();
+            }
+            return _instance;
+        }
+    }
+}

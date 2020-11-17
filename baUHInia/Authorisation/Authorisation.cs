47a88a3bf3ed2bd51095148b419cd6ef057e12ed
unknown <dotanek222@gmail.com>
2020-11-17 16:25:34 +0100

    Added map choice window for debug purposes.

--- baUHInia/Authorisation/Authorisation.cs
+++ baUHInia/Authorisation/Authorisation.cs
@@ -1,4 +1,5 @@
-﻿using baUHInia.Playground.View;
+﻿using baUHInia.MapLogic.View; // Exists for debug purposes only, remove later.
+using baUHInia.Playground.View;
 using System;
 using System.Windows.Forms;
 
@@ -31,6 +32,14 @@ namespace baUHInia.Authorisation
             Hide();
             //TODO: zmienisz potem :3
             AdminGameWindow app = new AdminGameWindow(null);
+            app.Show();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e) // Exists for debug purposes only, remove later.
+        {
+            Hide();
+            isMapVisible = true;
+            ChoiceWindow app = new ChoiceWindow();
             app.Show();
         }
     }

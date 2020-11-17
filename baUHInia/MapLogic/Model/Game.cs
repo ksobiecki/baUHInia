017e073a3cc5d32a0bdebb01ed9270b2f7b4e4c6
unknown <dotanek222@gmail.com>
2020-11-17 14:24:40 +0100

    Added not implemented methods.

--- baUHInia/MapLogic/Model/Game.cs
+++ baUHInia/MapLogic/Model/Game.cs
@@ -1,6 +1,13 @@
-﻿namespace baUHInia.MapLogic.Model
+﻿using baUHInia.Playground.Model;
+
+namespace baUHInia.MapLogic.Model
 {
     public class Game
     {
+        public int MapID { get; }
+        public int GameID { get; }
+        public int UserID { get; set; }
+        public Placement[] PlacedObjects { get; }
+        public Map Map { get; }
     }
 }
--- baUHInia/MapLogic/Model/Map.cs
+++ baUHInia/MapLogic/Model/Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using baUHInia.Playground.Model;
+using System.Collections.Generic;
 
 namespace baUHInia.MapLogic.Model
 {
@@ -9,21 +10,20 @@ namespace baUHInia.MapLogic.Model
         public int[,] TileGrid { get; }
         public bool[,] PlacableGrid { get; }
         public Dictionary<int, string> Indexer { get; }
-        //public GameObject[] AvailableTiles { get; }
+        public GameObject[] AvailableTiles { get; }
         public int AvailableMoney { get; set; }
-        //public Placement[] PlacedObjects { get; }
+        public Placement[] PlacedObjects { get; }
 
-        public Map(int mapID, int authorID, int[,] tileGrid, bool[,] placableGrid, Dictionary<int, string> indexer, int availableMoney)
+        public Map(int mapID, int authorID, int[,] tileGrid, bool[,] placableGrid, Dictionary<int, string> indexer, GameObject[] availableTiles, int availableMoney, Placement[] placedObjects)
         {
             MapID = mapID;
             AuthorID = authorID;
             TileGrid = tileGrid;
             PlacableGrid = placableGrid;
             Indexer = indexer;
-            // Available
+            AvailableTiles = availableTiles;
             AvailableMoney = availableMoney;
-            // Placed
-
+            PlacedObjects = placedObjects;
         }
     }
 }

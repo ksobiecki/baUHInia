aaadb381359200e4dd1ae20f55ece1a836b1f06e
Jarosław Drzymalski <jarek.drzymalski@gmail.com>
2020-11-26 02:29:04 +0100

    Logowanie zrobione w 90%
    Pozostało mi łączenie z bazą :)

--- baUHInia/Authorisation/Authorisation.cs
+++ baUHInia/Authorisation/Authorisation.cs
@@ -1,15 +1,19 @@
-using baUHInia.MapLogic.View; // Exists for debug purposes only, remove later.
+﻿using baUHInia.MapLogic.View; // Exists for debug purposes only, remove later.
 using baUHInia.Playground.View;
 using baUHInia.Database;
 
 using System;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
 
 namespace baUHInia.Authorisation
 {
     public partial class Authorisation : Form
     {
         BazaDanych bazaDanych = BazaDanych.GetBazaDanych();
+        public int secVal = 5;
 
         public Boolean isMapVisible = false;
         public Authorisation()
@@ -19,6 +23,7 @@ namespace baUHInia.Authorisation
 
         private void Authorisation_Load(object sender, EventArgs e)
         {
+            checkLock();
             Form DEBUG = new DEBUG.DEBUG();
             DEBUG.Show();
         }
@@ -31,21 +36,172 @@ namespace baUHInia.Authorisation
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void loginButton_Click(object sender, EventArgs e)
         {
-            isMapVisible = true;
-            Hide();
-            //TODO: zmienisz potem :3
-            AdminGameWindow app = new AdminGameWindow(null);
-            app.Show();
+            SHA256 mySHA256 = SHA256.Create();
+            String login;
+            String passwd;
+            login = loginLoginBox.Text;
+            if (loginLoginBox.Text.Length == 0)
+            {
+                MessageBox.Show("Proszę podać login.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (loginPasswordBox.Text.Length == 0)
+            {
+                MessageBox.Show("Wprowadzono puste hasło.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            passwd = ComputeSha256Hash(loginPasswordBox.Text);
+            Console.WriteLine(login);
+            Console.WriteLine(passwd);
+            if (true)
+            {
+                var result = MessageBox.Show("Pomyślnie zalogowano.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    LoginData ld = LoginData.GetInstance();
+                    ld.UserID = 0;
+                    ld.isAdmin = true;
+                    ld.name = login;
+                    ld.hash = passwd;
+                    Hide();
+                    AdminGameWindow app = new AdminGameWindow(null);
+                    app.Show();
+                }
+            }
+            else
+            {
+                secVal = secVal - 1;
+                if (secVal == 0)
+                {
+                    lockSystem();
+                }
+                else
+                {
+                    MessageBox.Show("Wprowadzone dane nie są poprawne.\nIlość prób do zablokowania systemu: " + secVal, "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    loginLoginBox.Text = "";
+                    loginPasswordBox.Text = "";
+                }
+                return;
+            }
         }
 
-        private void button2_Click_1(object sender, EventArgs e) // Exists for debug purposes only, remove later.
+        private void registerButton_Click(object sender, EventArgs e)
         {
-            Hide();
-            isMapVisible = true;
-            ChoiceWindow app = new ChoiceWindow();
-            app.Show();
+            SHA256 mySHA256 = SHA256.Create();
+            String login;
+            String passwd;
+            String passwd2;
+            login = registerLoginBox.Text;
+            passwd = registerPasswordBox.Text;
+            passwd2 = registerSecondPasswordBox.Text;
+            //czy w ogóle coś wpisali:
+            if (registerLoginBox.Text.Length == 0)
+            {
+                MessageBox.Show("Proszę podać login.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (registerLoginBox.Text.Length < 5)
+            {
+                MessageBox.Show("Login musi posiadać co najmniej 5 znaków.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (registerPasswordBox.Text.Length == 0)
+            {
+                MessageBox.Show("Wprowadzono puste hasło.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //czy takie samo:
+            if (passwd != passwd2)
+            {
+                MessageBox.Show("Wprowadzone hasła nie są takie same.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                registerLoginBox.Text = "";
+                registerPasswordBox.Text = "";
+                registerSecondPasswordBox.Text = "";
+                return;
+            }
+            //czy ma cyfre:
+            if ((!passwd.Any(c => char.IsDigit(c))) || (!passwd.Any(c => char.IsLower(c))) || (!passwd.Any(c => char.IsUpper(c))) || (passwd.Any(c => char.IsWhiteSpace(c))) || (passwd.Length < 8))
+            {
+                MessageBox.Show("Wprowadzone hasło:\n1. Musi zawierać co najmniej jedną cyfrę\n2. Musi zawierać co najmniej jedną wielką literę\n3. Musi zawierać co najmniej jedną małą literę\n4. Musi zawierać co najmniej 8 znaków\n5. Nie może zawierać spacji.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                registerPasswordBox.Text = "";
+                registerSecondPasswordBox.Text = "";
+                return;
+            }
+
+            passwd = ComputeSha256Hash(registerPasswordBox.Text);
+            Console.WriteLine(login);
+            Console.WriteLine(passwd);
+            if (true)
+            {
+                var result = MessageBox.Show("Pomyślnie zalogowano.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    LoginData ld = LoginData.GetInstance();
+                    ld.UserID = 0;
+                    ld.isAdmin = true;
+                    ld.name = login;
+                    ld.hash = passwd;
+                    Hide();
+                    AdminGameWindow app = new AdminGameWindow(null);
+                    app.Show();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Użytkownik z wybranym loginem już istnieje w systemie.", "Autoryzacja", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                registerLoginBox.Text = "";
+                registerPasswordBox.Text = "";
+                registerSecondPasswordBox.Text = "";
+                return;
+            }
         }
+        static string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void registerLoginBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox TB = (TextBox)sender;
+            int VisibleTime = 5000;  //in milliseconds
+
+            ToolTip tt = new ToolTip();
+            tt.Show("Login musi zawierać co najmniej 5 znaków", TB, 138, 0, VisibleTime);
+        }
+
+        private void registerPasswordBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox TB = (TextBox)sender;
+            int VisibleTime = 8000;  //in milliseconds
+
+            ToolTip tt = new ToolTip();
+            tt.Show("Wprowadzone hasło:\n1. Musi zawierać co najmniej jedną cyfrę\n2. Musi zawierać co najmniej jedną wielką literę\n3. Musi zawierać co najmniej jedną małą literę\n4. Musi zawierać co najmniej 8 znaków\n5. Nie może zawierać spacji.", TB, 138, 0, VisibleTime);
+        }
+
+        private void lockSystem()
+        {
+
+        }
+        private void checkLock()
+        {
+            if (false)
+            {
+                MessageBox.Show("Wprowadzono niepoprawne hasło zbyt wiele razy.\nProszę odczekać 5 minut.", "BLOKADA SYSTEMU", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+        }
+
     }
 }

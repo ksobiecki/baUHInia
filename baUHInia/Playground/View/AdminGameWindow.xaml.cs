4abab0a394e7fac7030c06fe82bf3e213b597da8
jakub_pietras <jacubeus@gmail.com>
2020-12-02 20:26:39 +0100

    Overhauled menu.

--- baUHInia/Playground/View/AdminGameWindow.xaml.cs
+++ baUHInia/Playground/View/AdminGameWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using baUHInia.Admin;
 using baUHInia.Authorisation;
 using baUHInia.MapLogic.Manager;
@@ -12,6 +14,7 @@ using baUHInia.Playground.Model.Resources;
 using baUHInia.Playground.Model.Selectors;
 using baUHInia.Playground.Model.Tiles;
 using baUHInia.Playground.Model.Wrappers;
+using baUHInia.Properties;
 
 namespace baUHInia.Playground.View
 {
@@ -24,11 +27,19 @@ namespace baUHInia.Playground.View
         private IAdminSelectorTabCreator _admin;
         private IGameMapManager _manager;
 
-        private UIElement _storedGui;
+        //private UIElement _storedGui;
+        private Grid MenuGrid { get; set; }
+        private Grid AdminGrid { get; set; }
+        private Grid SaveMapGrid { get; set; }
+        private Grid LoadMapGrid { get; set; }
+
+        private Grid GameMapGrid { get; set; }
+        //private Grid CurrentGrid { get; set; }
 
         public AdminGameWindow(LoginData credentials)
         {
             InitializeComponent();
+            StoreMenuGrid();
             AddLoadCardAndInitializeManager();
             AdjustWindowSizeAndPosition();
             InitializeProperties(credentials);
@@ -51,6 +62,14 @@ namespace baUHInia.Playground.View
         {
             ModeText.Text = text;
             ModeText.Foreground = color;
+        }
+
+        private void StoreMenuGrid() => MenuGrid = GameScroll.Content as Grid;
+
+        private void BackToGame()
+        {
+            SideGrid.Visibility = Visibility.Visible;
+            GameScroll.Content = GameMapGrid;
         }
 
         private void InitializeSelection()
@@ -88,11 +107,15 @@ namespace baUHInia.Playground.View
 
         private void CreateGameBoard()
         {
+            NewGameTitle.Text = "POWRÓT DO PLANSZY";
+            NewGameButton.Content = "POWRÓĆ";
             SideGrid.Visibility = Visibility.Visible;
             TileGrid = new Tile[BoardDensity, BoardDensity];
             TileObject tileObject = ResourceHolder.Get.GetTerrainTileObject("Plain Grass");
             _gameGridCreator = new PlacerGridCreator(this, BoardDensity, tileObject);
             _gameGridCreator.CreateGameGridInWindow(this, BoardDensity);
+            GameMapGrid = GameScroll.Content as Grid;
+            //CurrentGrid = GameMapGrid;
         }
 
         private void CreateSelectorGrid()
@@ -101,21 +124,13 @@ namespace baUHInia.Playground.View
             _selectorGridCreator = new AdminSelectorGridCreator(this, categories);
         }
 
-        private void SwapBoardAndAdminPanel()
-        {
-            UIElement temp = _storedGui;
-            _storedGui = GameScroll.Content as UIElement;
-            _storedGui.Visibility = Visibility.Visible;
-            temp.Visibility = Visibility.Visible;
-            GameScroll.Content = temp;
-        }
-
         //============================== INITIAL WINDOW ==============================//
 
         private void AddLoadCardAndInitializeManager()
         {
             _manager = new GameMapManager();
-            InitialMapGrid.Children.Add(_manager.GetMapLoadGrid());
+            //TODO:
+            //InitialMapGrid.Children.Add(_manager.GetMapLoadGrid());
         }
 
         private void InitializeProperties(LoginData credentials)
@@ -130,6 +145,12 @@ namespace baUHInia.Playground.View
 
         private void CreateNewMap(object sender, RoutedEventArgs e)
         {
+            if (GameMapGrid != null)
+            {
+                BackToGame();
+                return;
+            }
+
             InitializeSelection();
             CreateGameBoard();
             CreateSelectorGrid();
@@ -148,6 +169,31 @@ namespace baUHInia.Playground.View
             CategorySelector.SelectedIndex = 0;
             _selectorGridCreator.UpdateTileGroup(categories);
             _selectorGridCreator.CreateSelectionPanel(category, this);
+        }
+
+        private void CreateLoadWindow(object source, RoutedEventArgs args)
+        {
+            if (LoadMapGrid == null)
+            {
+                Border border = FindResource("LoadMapTemplate") as Border;
+                Grid loadGrid = new Grid {Children = {border}};
+                
+                Grid innerGrid = border.Child as Grid;
+                ((TextBlock) innerGrid.Children[0]).Text = "LISTA MAP";
+                ((Grid) innerGrid.Children[1]).Children.Add(_manager.GetMapLoadGrid());
+                ((Button) innerGrid.Children[3]).Click += (sender, arg) =>
+                {
+                    GameScroll.Content = GameMapGrid ?? MenuGrid;
+                    SideGrid.Visibility = GameMapGrid == null ? Visibility.Collapsed : Visibility.Visible;
+                };
+                ((Button) innerGrid.Children[4]).Click += (sender, arg) =>
+                {
+                    LoadMap();
+                };
+                LoadMapGrid = loadGrid;
+            }
+            SideGrid.Visibility = Visibility.Collapsed;
+            GameScroll.Content = LoadMapGrid;
         }
 
         private void LoadMap()
@@ -170,22 +216,29 @@ namespace baUHInia.Playground.View
         private void OpenSelectorTab(object source, RoutedEventArgs args)
         {
             //TODO change to make compatible with load/save windows
-            if (_storedGui == null)
+            SideGrid.Visibility = Visibility.Collapsed;
+            if (AdminGrid == null)
             {
                 _admin.GetReturnButton().Click += (sender, eventArgs) =>
                 {
                     AvailableObjects = _admin.GetModifiedAvailableObjects();
                     AvailableFounds = _admin.GetBudget();
-                    SwapBoardAndAdminPanel();
+                    GameScroll.Content = GameMapGrid;
+                    SideGrid.Visibility = Visibility.Visible;
+                    //SwapBoardAndAdminPanel();
                 };
-                _storedGui = GameScroll.Content as UIElement;
-                Grid grid = _admin.GetAdminSelectorTableGrid();
-                grid.VerticalAlignment = VerticalAlignment.Center;
-                grid.HorizontalAlignment = HorizontalAlignment.Center;
-                GameScroll.Content = grid;
-                _storedGui.Visibility = Visibility.Hidden;
+                //_storedGui = GameScroll.Content as UIElement;
+                AdminGrid = _admin.GetAdminSelectorTableGrid();
+                AdminGrid.VerticalAlignment = VerticalAlignment.Center;
+                AdminGrid.HorizontalAlignment = HorizontalAlignment.Center;
+                GameScroll.Content = AdminGrid;
+                //_storedGui.Visibility = Visibility.Hidden;
             }
-            else SwapBoardAndAdminPanel();
+            else
+            {
+                GameScroll.Content = AdminGrid;
+                //SwapBoardAndAdminPanel();
+            }
         }
 
         private void ChangeGameMode(object sender, RoutedEventArgs args)
@@ -193,6 +246,12 @@ namespace baUHInia.Playground.View
             UserGameWindow userWindow = new UserGameWindow {Owner = this};
             Hide(); // not required if using the child events below
             userWindow.ShowDialog();
+        }
+
+        private void ReturnToMenu(object sender, RoutedEventArgs args)
+        {
+            SideGrid.Visibility = Visibility.Collapsed;
+            GameScroll.Content = MenuGrid;
         }
 
         private void ReturnToLoginWindow(object sender, RoutedEventArgs args)
